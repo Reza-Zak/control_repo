d851ba2a84bd2008bd68eac225b8c66f286f020b
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2020-01-23 16:14:58 +1100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,5 @@
 node default {
- # file {'/root/readme':
- #    ensure => file,
- #    content => 'This is a Readme',
- # }
- # file { '/tmp/README.txt':
- #     ensure  => present,
- #     content => 'This a Readme file',
- #     owner   => root,
- #     group   => root,
- #     mode    => "644"
- #     
- # }
+}
+node 'pms.lab.local' {
+ include role::master_server
 }
