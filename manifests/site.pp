2439d7cb9772ab6814ea6e9020d28ad388da9c85
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2020-01-23 13:48:11 +1100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,4 +3,11 @@ node default {
      ensure => file,
      content => 'This is a Readme',
   }
+  file { '/tmp/README.txt':
+      ensure => present,
+      content => 'This a Readme file',
+      owner => root,
+      group => root,
+      
+  }
 }
