584e47c531212e0a6db98df5dd3d35920e59d8b1
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2020-01-29 12:09:06 +1100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,10 @@
 node default {
  include role::master_server
 }
+node 'pag.lab.local' {
+ include role::master_server
+ include role::minecraft_server
+}
 node 'pms.lab.local' {
  include role::master_server
 }
