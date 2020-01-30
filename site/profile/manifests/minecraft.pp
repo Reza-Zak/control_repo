8a8da58f0e42f8e8d2f7d65d2dd8392afb3a8698
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2020-01-30 14:43:14 +1100

    Update minecraft.pp

--- site/profile/manifests/minecraft.pp
+++ site/profile/manifests/minecraft.pp
@@ -1,6 +1,6 @@
 class profile::minecraft {
-  #include minecraft
-  class {'minecraft':
-    install_dir => '/srv/minecraft',
-  }
+  include minecraft
+  #class {'minecraft':
+  #  install_dir => '/srv/minecraft',
+  #}
 }
