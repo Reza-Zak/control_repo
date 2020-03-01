67581c079556596f9dc06b3b83b24feaf9cbb4c7
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2020-03-01 15:36:29 +1100

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,9 @@
+node 'pag.lab.local' {
+        package { 'httpd':
+                ensure => installed,
+        }
+        class {'resolv_conf' :
+                nameservers => ['192.168.80.2','8.8.8.8','4.2.2.4'],
+                domainname => 'lab.local',
+        }
+}
