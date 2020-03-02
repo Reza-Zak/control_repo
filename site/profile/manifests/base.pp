24ee9ad0901ba60085c77a17258f2e8548fe843b
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2020-03-03 10:01:54 +1100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -5,7 +5,7 @@ class profile::base {
   package { 'openldap-clients':
     ensure => installed,
     }
-  class {'resovle_conf':
+  class {'resolv_conf':
     nameservers => ['192.168.80.2','4.2.2.4','8.8.8.8'],
     domainname  => 'lab.local',
     }
