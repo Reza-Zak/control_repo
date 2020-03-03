a566e9d3cdb6f3699dc87ad9fef83bd11c03cd24
Reza-Zak <59948055+Reza-Zak@users.noreply.github.com>
2020-03-03 22:51:10 +1100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,7 +2,10 @@ class profile::base {
   package { ['vim','openldap-clients']:
     ensure => installed,
     }
-  class {'resolv_conf':
+  user { 'reza':
+    ensure => present,
+    }
+  class { 'resolv_conf':
     nameservers => ['192.168.80.2','4.2.2.4','8.8.8.8'],
     domainname  => 'lab.local',
     }
